626f90725670718c9d38529ef74a6d43f3e56e1f
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-10 16:24:57 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (60.47%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,27 @@
-class Solution {// Using Map & without sort
-public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>freq;
-        vector<int>ans;
-        for(int i = 0;i<nums1.size();i++){
-            freq[nums1[i]]++;
-        }
-        for(int i = 0;i<nums2.size();i++){
-            if (freq[nums2[i]] > 0){
-                freq[nums2[i]]--; 
-                ans.push_back(nums2[i]);
-            }
-        }
-        return ans;
-    }
+// class Solution {// Using Map & without sort
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//         map<int,int>freq;
+//         vector<int>ans;
+//         for(int i = 0;i<nums1.size();i++){
+//             freq[nums1[i]]++;
+//         }
+//         for(int i = 0;i<nums2.size();i++){
+//             if (freq[nums2[i]] > 0){
+//                 freq[nums2[i]]--; 
+//                 ans.push_back(nums2[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+class Solution {
+    public:
+    vector<int> intersect(vector<int>& a, vector<int>& b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
+    return a;
+}
 };
+
