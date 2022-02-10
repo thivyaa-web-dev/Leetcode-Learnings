2df34ca569a40d292a42241a3f7df4da0a031c07
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-10 20:16:32 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (45.80%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,25 @@
+// Concept
+// First, we have some edge cases.
+// Then, we use simple DP: for each house, we can't rob two adjacent houses. So either we rob the previous one - dp[i-1], or the two before and the current one - nums[i]+dp[i-2].
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //edge cases 
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0],nums[1]);
+        
+        //dyanamic programming, decide each problem by its subproblem
+        int dp[nums.size()];
+        dp[0] =nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i =2;i<nums.size();i++)
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+        return dp[nums.size()-1];                
+        
+    }
+};
