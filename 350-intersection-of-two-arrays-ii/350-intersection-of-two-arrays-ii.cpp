d278355f6b754f3b1b133f2759b99e49c51257b6
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-10 16:13:23 +0530

    Time: 3 ms (93.68%), Space: 10.7 MB (19.89%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -3,20 +3,21 @@
 // Then, while iterating nums2, we count how many numbers we have in common.
 
 
-class Solution {// Using Map & without sort
+class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>freq;
-        vector<int>ans;
-        for(int i = 0;i<nums1.size();i++){
-            freq[nums1[i]]++;
-        }
-        for(int i = 0;i<nums2.size();i++){
-            if (freq[nums2[i]] > 0){
-                freq[nums2[i]]--; 
-                ans.push_back(nums2[i]);
+        unordered_map<int, int> freq;
+        vector<int> res;
+        
+        for (auto num : nums1) freq[num]++;
+        
+        for (auto num : nums2) {
+            if (freq[num]) {
+                res.push_back(num);
+                freq[num]--;
             }
         }
-        return ans;
+        
+        return res;
     }
 };
