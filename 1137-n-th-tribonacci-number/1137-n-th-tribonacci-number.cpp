fbf2cb59e2b173eb503fef85973a1b608e0229d6
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-10 18:00:06 +0530

    Time: 3 ms (22.12%), Space: 5.7 MB (97.22%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int dp[40]; // due to the constraints till 40
+        dp[0] =0;
+        dp[1] =1;
+        dp[2] =1;
+        
+        for(int i =3;i<=n;i++){
+            dp[i] =dp[i-3]+dp[i-2]+dp[i-1];
+            
+        }
+        return dp[n];
+
+        
+        
+    }
+};
