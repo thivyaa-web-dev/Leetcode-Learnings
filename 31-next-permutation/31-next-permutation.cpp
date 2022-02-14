1baa6752a7d57cde1127253a9a6b373a1a224579
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-14 23:10:49 +0530

    Time: 4 ms (76.97%), Space: 12 MB (75.32%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+      next_permutation(begin(nums), end(nums));
+    // in reverse order find the number in increasing trend 
+        
+
+    }
+};
