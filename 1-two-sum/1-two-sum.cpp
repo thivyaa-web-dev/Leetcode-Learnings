6767af6f4334cd15ac0feb94cd32edbff78b51fe
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-27 14:42:01 +0530

    Time: 12 ms (85.60%), Space: 10.9 MB (29.01%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> ans; 
         unordered_map<int, int> mpp; 
         for(int i = 0;i<nums.size();i++) {
+            //if it exists in the hastable it will never point outside of the hashtable( it will never point at the last element at the hashtable)
             if(mpp.find(target - nums[i]) != mpp.end()) {
                 ans.push_back(mpp[target-nums[i]]); 
                 ans.push_back(i); 
