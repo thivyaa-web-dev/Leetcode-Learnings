5b954963c88034401a3a660ef7496c97a280d7ca
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-02-27 15:36:40 +0530

    Time: 133 ms (50.25%), Space: 53.5 MB (19.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
         unordered_set<int> set;// can take only unique elements
-        for(auto x:nums)
-            set.insert(x);
-        if(set.size() == n)
-        return false;
+        for(auto x:nums) //iterate them through
+            set.insert(x); // you need to add them
+        if(set.size() == nums.size())
+        return false; 
         else return true;
         
     }
