6296caa0139898a109d9c532501e46bb4c227b47
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-03-08 18:36:15 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int N)
+    {
+      //Your code here
+    //Your code here
+      unordered_map<int,int> map;
+      for(int i=0;i<N;i++){
+          map[arr[i]]=1;
+      }
+      int max=0;
+    for(int i=0;i<N;i++)
+    {
+        int x=arr[i];
+        int j=0;
+        if(map.count(x-1)>0)
+            continue;
+        while(map.count(x)>0 )
+        {
+                
+            j++;
+            x++;
+        }
+        if(j>max)
+        {  
+            max=j;
+        }   
+    }
+    
+    return max;
+    }
+};
+
+// { Driver Code Starts.
+ 
+// Driver program
+int main()
+{
+ int  t,n,i,a[100001];
+ cin>>t;
+ while(t--)
+ {
+  cin>>n;
+  for(i=0;i<n;i++)
+  cin>>a[i];
+  Solution obj;
+  cout<<obj.findLongestConseqSubseq(a, n)<<endl;
+ }
+      
+    return 0;
+}  // } Driver Code Ends
