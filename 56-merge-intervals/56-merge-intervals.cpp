d30a3f8350a4bb665ba89fabf0d69df83fdef33d
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-03-11 21:03:02 +0530

    Time: 36 ms (89.71%), Space: 19.9 MB (32.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()<=1) return intervals;
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>>output;
-        output.push_back(intervals[0]);
-        for(int i =1;i<intervals.size();i++){
-if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
-        else output.push_back(intervals[i]); 
+        vector<vector<int>> mergedIntervals;
+        //if we are not given any interval, in that case we need to return empty ans 
+        // please be concerened of the edge cases 
+        if(intervals.size() == 0){
+            return mergedIntervals;
         }
-        return output;
+        sort(intervals.begin(), intervals.end());
+        //we take the vector int and put the first interval into it 
+        vector<int>tempInterval = intervals[0];
+        
+        //lineraly traverse through the intervals given to us 
+        for(auto it :intervals){
+            if(it[0] <=tempInterval[1]){
+                tempInterval[1] = max(it[1], tempInterval[1]);
+            } else{
+                mergedIntervals.push_back(tempInterval);
+                tempInterval = it;
+            }
+        }
+        mergedIntervals.push_back(tempInterval);
+        return mergedIntervals;
     }
 };
