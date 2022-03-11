487661eed907afd3c8d56672e03456b9a2d695d2
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-03-11 19:05:34 +0530

    Time: 136 ms (50.13%), Space: 23.7 MB (25.49%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        if(n < 3) return {};
-        vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n-2; ++i){
-            if(i == 0 || nums[i] != nums[i-1]){
-                int j = i + 1, k = n-1;
-                while(j < k){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if(sum == 0){
-                        result.push_back({nums[i], nums[j], nums[k]});
-                        while(j < k && nums[j] == nums[j+1]) j++;
-                        while(j < k && nums[k] == nums[k-1]) k--;
-                        j++; k--;
-                    }
-                    else if (sum > 0) k--;
-                    else j++;
-                }
+    vector<vector<int>> threeSum(vector<int>& num) {
+        vector<vector<int>> res; 
+        sort(num.begin(), num.end()); 
+        
+        // moves for a
+        for (int i = 0; i < (int)(num.size())-2; i++) {
+            
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                
+                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
+                
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        
+                        vector<int> temp; 
+                        temp.push_back(num[i]); 
+                        temp.push_back(num[lo]); 
+                        temp.push_back(num[hi]); 
+                        res.push_back(temp);
+                        
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } 
+                    else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
             }
         }
-        return result;
+        return res;
     }
-        
-    
 };
