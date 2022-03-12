802d319f3664f2dfff396c350e4814220f86ffee
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-03-12 16:15:01 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.16%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,15 +8,19 @@ public:
         //edge cases 
         if(nums.size() == 0) return 0;
         if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[0],nums[1]);
+    //if(nums.size() == 2) return max(nums[0],nums[1]);
         
         //dyanamic programming, decide each problem by its subproblem
-        int dp[nums.size()];
-        dp[0] =nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i =2;i<nums.size();i++)
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        return dp[nums.size()-1];                
+        int dp[nums.size()+1]; //intialize a dp memo table 
+        dp[0] =0;
+        dp[1] = nums[0];
+       // dp[1] = max(nums[0],nums[1]);
+        for(int i =1;i<nums.size();i++)
+            //max at the next step
+            //we're calculating a new max
+            //old max and the new house
+            dp[i+1] = max(dp[i], nums[i]+dp[i-1]);
+        return dp[nums.size()];                
         
     }
 };
