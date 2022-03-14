2694ac3d2fb440bb00e87914fff0bfb389c26629
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-03-14 12:36:55 +0530

    Time: 11 ms (59.88%), Space: 10.6 MB (96.52%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,26 +1,33 @@
 class Solution {
-
-      void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans){
-        if(currSum>target) return; //backtrack
-        if(currSum==target){
-            ans.push_back(currComb); //store the solution and backtrack
+    private:
+    void findCombination(int ind,int target,vector<int>& arr,vector<vector<int>>&ans,vector<int>&ds){
+//base case
+        if(ind == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
             return;
         }
-          for( int i= currIndex;i<candidates.size();i++ ){ //try all the possible options for the next level
-              currComb.push_back(candidates[i]);
-              currSum += candidates[i];
-            combination(candidates, target, currComb, currSum, i, ans); //try with this combination, whether it gives a solution or not.
-            currComb.pop_back(); //when this option backtrack to here, remove this and go on to the next option.
-            currSum-=candidates[i];
-              
-          }
-       
+        //pick up the element 
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findCombination(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+            
+        }
+          findCombination(ind+1,target,arr,ans,ds);
+        
+        
+        
     }
-     public:
+    
+  
+    
+public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> currComb;
-        combination(candidates, target, currComb, 0, 0, ans);
+        vector<vector<int>>ans;
+        vector<int>ds; //initialize data structure to be used in the recursion
+        findCombination(0,target,candidates,ans,ds);
         return ans;
         
         
