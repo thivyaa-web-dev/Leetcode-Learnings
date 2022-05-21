41f7cc21af24e3cbd0c75ce989e3e221fdbb7ed5
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-05-21 16:38:17 +0530

    Time: 43 ms (25.10%), Space: 10.8 MB (64.54%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,12 +1,23 @@
+//this is the solution for the memoization code 
+
 class Solution {
 public:
-    int longestCommonSubsequence(string &a, string &b) {
-        short m[1001][1001] = {};
-        for(auto i=0;i<a.size();i++)
-            for(auto j=0;j<b.size();j++)
-                m[i+1][j+1] =a[i]==b[j] ?m[i][j]+1:max(m[i+1][j],m[i][j+1]);
-        return m[a.size()][b.size()];
+    int dp[1000+1][1000 +1];
+    int lcs(string &s1, string &s2, int n, int m) {
+        if(n==0||m==0)
+            return 0;
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        if(s1[n-1] == s2[m-1])
+            return dp[n][m] = 1+lcs(s1,s2,n-1,m-1);
+        else
+            return dp[n][m] = max(0+lcs(s1,s2,n,m-1),0+lcs(s1,s2,n-1,m));
         
         
+    }
+    int longestCommonSubsequence(string text1,string text2)
+    {
+        memset(dp,-1,sizeof(dp));
+        return lcs(text1,text2,text1.length(),text2.length());
     }
 };
