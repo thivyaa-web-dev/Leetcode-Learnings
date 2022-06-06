bfbf523baf8b1c0269e28e316e675ddc22bba6b3
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-06 14:37:15 +0530

    Time: 4 ms (38.76%), Space: 8.4 MB (42.31%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,19 +11,36 @@
  */
 class Solution {
 public:
-    void in(TreeNode* root, vector<int> &v){
-        if(root==NULL){
-            return;
-        }
-        else{
-            in(root->left,v);
-            v.push_back(root->val);
-            in(root->right,v);
-        }
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>v;
-        in(root,v);
-        return v;
+        //first, we create a stack 
+        stack<TreeNode*>st;
+        //create a node and assign it to root 
+        TreeNode*node = root;
+        //declare a vector which stores inorder 
+        vector<int>inorder;
+        while(true){
+        //if the node that is the root is not null, we take that and insert it into stack
+            if(node != NULL){
+                st.push(node);
+        //after pushing into stack, move to the left 
+                node = node->left;
+            }
+            else{
+            //if you traverse a tree and end up getting null, this is what you need to do 
+            //if stack is empty there are no nodes to travel
+                if(st.empty() == true) break;
+            //else if the st is not empty, whatever is at the top we will take it 
+                node = st.top();
+                st.pop();
+            //and this is eventually inorder
+                inorder.push_back(node->val);
+                node = node->right;
+            }
+                
+        }
+        return inorder;
     }
 };
+
+//TC :O(n) , as we traverse every node 
+// SC: O(n), as we use a auxilary space
