acf2ab91c67c6a42102e29b3ff37b9ac509b1624
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-06 13:54:30 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (93.65%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,26 +11,34 @@
  */
 class Solution {
 public:
-
-//declaring a global variable *ans because we dont have to keep declaring it again and again.
-   vector<int> ans;
-    
-//find preorder sequence
-    void preorder(TreeNode* root){
-        if(root == NULL){
-            return;
+    vector<int> preorderTraversal(TreeNode* root) {
+        //we need to declare a vector 
+     vector<int> preorder;
+        //if the tree is empty , you return empty preorder 
+        if(root == NULL) return preorder;
+        
+        
+        //you then initialize the stack 
+        stack<TreeNode*>st;
+        st.push(root);
+        
+        //you keep iterating on the stack till it is non-empty 
+        while(!st.empty()){
+        //now you get the top most element 
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+        //check if it has a right or left on it and put it into the stack 
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+             if(root->left!=NULL){
+                st.push(root->left);
+            }
+            
+            
         }
         
-        ans.push_back(root ->val);
-        preorder(root->left);
-        preorder(root->right);
-    }
-    
-    
-    
-    vector<int> preorderTraversal(TreeNode* root) {
-        preorder(root);
-        return ans;
-        
+        return preorder;
     }
 };
