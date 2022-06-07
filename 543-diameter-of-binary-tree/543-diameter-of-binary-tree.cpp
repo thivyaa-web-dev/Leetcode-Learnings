baa1acc8f25dfdff0b612e0ba7357e957d558744
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-07 16:12:29 +0530

    Time: 7 ms (94.57%), Space: 20.3 MB (64.74%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -10,24 +10,24 @@
  * };
  */
 class Solution {
-	public:
-int ans=0;
-
-int height(TreeNode* root)
-{
-    if(!root) return 0;
+public:
+    int ans =0; //we need to declare the ans variable gloabally 
     
-    int lHeight = height(root->left);
-    int rHeight = height(root->right);
+    //we now need to find the height of the tree 
     
-    ans= max(ans, 1 + lHeight + rHeight);
-    return 1+ max( lHeight , rHeight);
-
-}
-
-int diameterOfBinaryTree(TreeNode* root) {
-    if(!root) return 0;
-    height(root);
-    return ans-1;
-}
+    int height(TreeNode* root) {
+        if(!root) return 0;
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        ans = max(ans, 1+lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode*root){
+        if(!root) return ans; //if there is not even a root, there is not even a tree that exists 
+        height(root);
+        return ans-1;
+        
+    }
 };
