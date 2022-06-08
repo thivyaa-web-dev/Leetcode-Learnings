155e058fbe7ea1bcaf75aa673e9d5e333fc68996
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-08 13:29:59 +0530

    Time: 7 ms (34.43%), Space: 12 MB (93.12%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -19,16 +19,26 @@ public:
 
   queue < TreeNode * > nodesQueue;
   nodesQueue.push(root);
+//we are using a flag, that is initialized to be true 
+//true means leftoright
+//false means rightoleft
   bool leftToRight = true;
+        
+        
+//we need to now traverse in the queue 
 
   while (!nodesQueue.empty()) {
     int size = nodesQueue.size();
     vector < int > row(size);
+    //we need to run a for loop for all the nodes of that level
     for (int i = 0; i < size; i++) {
       TreeNode * node = nodesQueue.front();
       nodesQueue.pop();
 
       // find position to fill node's value
+      //depending on the flagLeftoright, we can either insert it from the front or the back
+      //only thing that is made sure is if to push it from the front or from the back
+        
       int index = (leftToRight) ? i : (size - 1 - i);
 
       row[index] = node -> val;
