e9711216ec88f69b78f230d9ac6ce58e69be6701
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-19 11:27:19 +0530

    Added solution - LeetHub

---
+++ Reverse a sublist of a linked list - GFG/reverse-a-sublist-of-a-linked-list.cpp
@@ -0,0 +1,123 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Link list node 
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* reverseBetween(Node* head, int m, int n)
+    {
+        //code here 
+        int cnt =1;
+        Node*temp = head;
+        Node*last = NULL;
+        
+        while(cnt<m){
+            last = temp;
+            temp = temp->next;
+            cnt++;
+        }
+        Node*pre = NULL;
+        Node*cur = temp;
+        Node*toLink = temp;
+        
+        
+        while(cnt<=n){
+            Node*next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+            cnt++;
+        }
+        if(last == nullptr){
+            head ->next=cur;
+            return pre;
+        }
+        last->next = pre;
+        toLink->next = cur;
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, m, n;
+		cin >> N>>m>>n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		
+
+        Solution ob;
+
+		Node* newhead = ob.reverseBetween(head, m, n);
+		printList(newhead);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+  // } Driver Code Ends
