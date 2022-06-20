89cf1fb77d75abba01ce328f406c26d18bf2aa20
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-20 21:35:31 +0530

    Time: 3 ms (39.68%), Space: 6.8 MB (87.05%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i < q.size()-1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int r = q.front();
+        q.pop();
+        return r;
+    }
+    
+    int top() {
+       return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
