6b8d2febb5d1619f1482b2144532cb8d10561f00
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-06-29 12:23:40 +0530

    Time: 68 ms (24.62%), Space: 71.5 MB (11.46%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,25 +11,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode*ans = new ListNode(0);
-        ListNode*p = l1,*q=l2,*curr = ans;
+    //create a dummy node 
+        ListNode*dummy = new ListNode();
+    //create a temp which copies dummy node 
+        ListNode*temp = dummy;
         int carry =0;
-        while(p!=NULL || q!=NULL){
-            int x = (p!= NULL) ?p->val:0;
-            int y = (q!=NULL)? q->val:0;
-            int sum = carry+x+y;
+//we are going to iterate till either of them become null or zero 
+        while(l1!= NULL || l2!=NULL || carry){
+            int sum =0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+              if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
             carry = sum/10;
-            curr->next = new ListNode(sum%10);
-            curr= curr->next;
+            ListNode*node = new ListNode(sum%10);
+                temp->next = node;
+            temp = temp->next;
             
-            if(p!=NULL) p = p->next;
-            if(q!=NULL) q = q->next;
+            
             
         }
-        if(carry>0){
-            curr->next = new ListNode(carry);
-        }
+      return dummy->next;
 
-        return ans->next;
     }
 };
