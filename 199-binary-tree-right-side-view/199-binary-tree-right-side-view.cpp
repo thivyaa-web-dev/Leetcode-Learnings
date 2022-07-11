25a7ac29b9371269f0cc599ed3d03b6db2781d0e
Thivyaa Mohan <63237654+thivyaa-web-dev@users.noreply.github.com>
2022-07-11 15:40:46 +0530

    Time: 3 ms (84.17%), Space: 12 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,20 +11,17 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int height) {
-        if (!root) return;
-        if (height == res.size())
-            res.push_back(root->val);
-        dfs(root->right, height + 1);
-        dfs(root->left, height + 1);
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()<level) res.push_back(root->val);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
     }
     
-    vector<int> rightSideView(TreeNode* root) {
-        dfs(root, 0);
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> res;
+        recursion(root, 1, res);
         return res;
     }
-
-private:
-    vector<int> res;
 };
-
